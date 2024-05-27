73a2d4a6b516f814b89ccbab3d14dad72566d91e
Einanas <144671961+Einanas@users.noreply.github.com>
2024-05-27 13:17:49 +0200

    added helpers + server GET code
    Alleen profielen moet er nog bij

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(express.urlencoded({extended: true }))
 // Stel het oba profile in
 const apiData = 'https://fdnd-agency.directus.app/items/oba_profile'
 
-// Stel het oba profile in
+// Stel het oba family in
 const apiFamily = 'https://fdnd-agency.directus.app/items/oba_family'
 
 // Stel het basis endpoint in
@@ -31,17 +31,62 @@ const apiItem = (apiUrl + 'oba_item')
 
 // Home pagina
 app.get('/', function(request, response) {
-    console.log(savedItems)
+    // console.log(savedItems)
     fetchJson('https://fdnd-agency.directus.app/items/oba_item').then((items) => {
-        response.render('index', {           
-            items: items.data, /*hier zeg ik dat iedereen getoond moet worden*/
+        response.render('home', {           
+            // items: items.data, /*hier zeg ik dat iedereen getoond moet worden*/
             // savedItems: savedItems
         })
     });
 })
 
-// Hier moet de rest van de GET, POST en ROUTE komen
-app.get()
+// Betalingen pagina
+app.get('/betalingen', function(request, response){
+    fetchJson('https://fdnd-agency.directus.app/items/oba_item')
+        .then((itemsDataUitDeAPI) => {
+            // Check if itemsDataUitDeAPI.data is an array
+            if (Array.isArray(itemsDataUitDeAPI.data)) {
+                response.render('betalingen', {
+                    items: itemsDataUitDeAPI.data
+                });
+            } else {
+                console.error("Invalid data format from API");
+                response.status(500).send("Internal Server Error: Invalid data format");
+            }
+        })
+});
+
+// Detail Pagina 
+app.get('/detail', function(request, response){
+    console.log()
+    fetchJson('https://fdnd-agency.directus.app/items/oba_item').then((itemsDataUitDeAPI) => {
+        response.render('detail', {
+            items: itemsDataUitDeAPI.data[0]
+        })
+    })
+})
+
+// Favorieten pagina
+app.get('/favorieten', function(request, response){
+    console.log()
+    fetchJson('https://fdnd-agency.directus.app/items/oba_item').then((itemsDataUitDeAPI) => {
+        response.render('favorieten', {
+            items: itemsDataUitDeAPI.data[0]
+        })
+    })
+})
+
+
+// Uitleningen pagina
+app.get('/uitleningen', function(request, response){
+    console.log()
+    fetchJson('https://fdnd-agency.directus.app/items/oba_item').then((itemsDataUitDeAPI) => {
+        response.render('uitleningen', {
+            items: itemsDataUitDeAPI.data[0]
+        })
+    })
+})
+
 
 // Stel het poortnummer in waar express op moet gaan luisteren
 app.set('port', process.env.PORT || 8000)
