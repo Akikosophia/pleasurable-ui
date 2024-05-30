86a5410a2c302744a51964c14b49d722b0553ab3
Akikosophia <144008863+Akikosophia@users.noreply.github.com>
2024-05-30 12:42:30 +0200

    Profielen pagina
    De code structuur gegeven

--- public/script.js
+++ public/script.js
@@ -1,8 +1,8 @@
-const bell = document.querySelector('svg')
+const bell = document.querySelector("svg.custom-bell")
 
-bell.addEventListener('click', jumpHandler)
-bell.addEventListener('animationend', jumpHandler)
+bell.addEventListener('click', jumpBell)
+bell.addEventListener('animationend', jumpBell)
 
-function jumpHandler() {
+function jumpBell() {
  bell.classList.toggle('jump')
 }
